1a298dad25cebd1085b54807be589c2aaa4d898e
lo219 <16652314+lo219@users.noreply.github.com>
2019-02-19 08:33:25 +0000

    adding python function that creates a table in a db from a csv file

--- create_database_from_csv.py
+++ create_database_from_csv.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def create_database_from_csv(db_name, table_name, db_type, csv_file_path):
-    connection_string = db_type + ':///' + dbname
+    connection_string = db_type + ':///' + db_name
     engine = create_engine(connection_string)
     #connection = engine_connect()
     with open(csv_file_path, 'r') as file:
         csv_df = pd.read_csv(file)
-    csv_df.to_sql(table_name, con=engine, index=True, index_lable='id'. if_exists='replace')
+    csv_df.to_sql(table_name, con=engine, index=True, index_label='id', if_exists='replace')
---
+++ test_function.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import create_database_from_csv
+
+create_database_from_csv.create_database_from_csv('test.db', 'google', 'sqlite', 'google_stock_data.csv')
